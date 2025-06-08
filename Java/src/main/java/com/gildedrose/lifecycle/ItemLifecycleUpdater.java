7368e02feea57af5b2e3d307100fad8d28b89168
ext.artem.trybel <ext.artem.trybel@kyriba.com>
2025-06-08 20:55:43 +0300

    Create items object resolver. Remove business logic method from ItemLifecycleUpdater. Using factory pattern instead of if conditions.

--- Java/src/main/java/com/gildedrose/lifecycle/ItemLifecycleUpdater.java
+++ Java/src/main/java/com/gildedrose/lifecycle/ItemLifecycleUpdater.java
@@ -1,8 +1,11 @@
 package com.gildedrose.lifecycle;
 
-import com.gildedrose.model.Item;
+import com.gildedrose.model.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+import java.util.function.Function;
 
 import static com.gildedrose.constant.ItemsNameConstant.*;
 
@@ -12,6 +15,14 @@ public class ItemLifecycleUpdater {
 
     private final Item[] items;
 
+    private static final Map<String, Function<Item, AbstractItem>> ITEM_RESOLVER = Map.of(
+        AGED_BRIE, AgedBrieItem::new,
+        BACKSTAGE_PASSES, BackstagePassItem::new,
+        CONJURED_MANA_CAKE, ConjuredItem::new,
+        SULFURAS, SulfurasItem::new
+    );
+
+
     public void processDailyChange() {
         for (Item item : items) {
             processItemDailyChange(item);
@@ -19,56 +30,8 @@ public class ItemLifecycleUpdater {
     }
 
     private void processItemDailyChange(Item item) {
-        if (!item.name.equals(SULFURAS)) {
-            item.sellIn--;
-        }
-
-        if (item.name.equals(AGED_BRIE)) {
-            updateAgedBrie(item);
-        } else if (item.name.equals(BACKSTAGE_PASSES)) {
-            updateBackstagePass(item);
-        } else if (item.name.equals(CONJURED_MANA_CAKE)) {
-            updateConjuredItem(item);
-        } else if (!item.name.equals(SULFURAS)) {
-            updateDefaultItem(item);
-        }
-    }
-
-    private void updateAgedBrie(Item item) {
-        increaseQuality(item);
-        if (item.sellIn < 0) increaseQuality(item);
-    }
-
-    private void updateBackstagePass(Item item) {
-        increaseQuality(item);
-        if (item.sellIn < 10) increaseQuality(item);
-        if (item.sellIn < 5) increaseQuality(item);
-
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        }
-    }
-
-    private void updateDefaultItem(Item item) {
-        decreaseQuality(item);
-        if (item.sellIn < 0) decreaseQuality(item);
-    }
-
-    private void updateConjuredItem(Item item) {
-        decreaseQuality(item);
-        decreaseQuality(item);
-        if (item.sellIn < 0) {
-            decreaseQuality(item);
-            decreaseQuality(item);
-        }
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) item.quality++;
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) item.quality--;
+        AbstractItem wrappedItem = ITEM_RESOLVER.getOrDefault(item.name, DefaultItem::new).apply(item);
+        wrappedItem.update();
     }
 
     public Item getItem(int index) {

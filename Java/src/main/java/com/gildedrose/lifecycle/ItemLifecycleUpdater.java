8aee90acbadc27d2a5a8521ef8f94c24faa9eaf6
ext.artem.trybel <ext.artem.trybel@kyriba.com>
2025-06-09 17:11:51 +0300

    Include lig4j into project. Add log debug into updater class.

--- Java/src/main/java/com/gildedrose/lifecycle/ItemLifecycleUpdater.java
+++ Java/src/main/java/com/gildedrose/lifecycle/ItemLifecycleUpdater.java
@@ -4,18 +4,24 @@ import com.gildedrose.enums.ItemType;
 import com.gildedrose.model.AbstractItem;
 import com.gildedrose.model.Item;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 @Getter
 public class ItemLifecycleUpdater {
 
     public void processDailyChange(List<Item> items) {
+        log.debug("Starting processing of daily change for the following items: {}", items);
         items.forEach(this::processItemDailyChange);
+        log.debug("Finished processing of daily change.");
     }
 
     private void processItemDailyChange(Item item) {
+        log.debug("Starting processing of daily change for the following item: {}", item);
         AbstractItem wrappedItem = ItemType.applyFromItem(item);
         wrappedItem.update();
+        log.debug("Finished processing of daily change. Result item: {}", item);
     }
 }
